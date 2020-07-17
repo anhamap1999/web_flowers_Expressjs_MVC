97b957ae4bb238a04140aa74d77218617982b206
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-07-17 16:23:13 +0700

    Use promise for auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,24 +3,20 @@ const User = require('../models/user');
 const dotenv = require('dotenv').config();
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
-exports.isAuth = (req, res, next) => {
+exports.isAuth = async (req, res, next) => {
     //var token = req.headers["x-access-token"];
     const token = req.header('Authorization').replace('Bearer ', '');
-    if (token) {        
-        jwtHelper.verifyToken(token, accessTokenSecret)
-            .then(decoded => {
-                User.findById(decoded.data._id)
-                    .then(user => {
-                        req.jwtDecoded = decoded;
-                        req.role = user.role;
-                        req.user = user;
-                        next();
-                    });
-            })
-            .catch(error => {
-                res.status(401).json({message: "Unauthorized!"});
-            });
+    if (token) {
+        try {
+            const decoded = await jwtHelper.verifyToken(token, accessTokenSecret);
+            const user = await User.findOne({ _id: decoded.data._id, status: 'active' });
+            req.jwtDecoded = decoded;
+            req.user = user;
+            next();
+        } catch (error) {
+            res.status(401).json({ message: "Unauthorized!" });
+        }
     } else {
-        res.status(400).json({message: "No token provided!"});
+        res.status(400).json({ message: "No token provided!" });
     }
 }
