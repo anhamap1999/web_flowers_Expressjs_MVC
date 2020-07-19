6bec30d4ac5132459b201a67321782340001518c
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-07-19 16:09:21 +0700

    Use promise for cart.controllers.js

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -1,107 +1,71 @@
 const Cart = require('../models/cart');
 
-exports.listCarts = (req, res) => {
-    Cart.find({}, (err, carts) => {
-        if (err) {
-            res.status(500).json(err);
-        } else if (carts) {
-            res.status(200).json({carts: carts});
-        } else {
-            res.status(404).json({message: 'Cart not found!'})
-        }        
-    });
-};
-
-exports.getCart = (req, res) => {
-    //Cart.findOne({username: req.jwtDecoded.data.username}, (err, cart) => {
-    Cart.findOne({username: req.role === "admin" ? req.params.username : req.jwtDecoded.data.username}, (err, cart) => {
-        if (err) {
-            res.status(500).json(err);
-        } else if (cart) {
-            res.status(200).json({cart: cart});
-        } else {
-            res.status(404).json({message: 'Cart not found!'})
-        }        
-    });
-};
-
-exports.addItem = (req, res) => {
-    const {flower_id, quantity, unit_price} = req.body;
-    //Cart.findOne({username: req.jwtDecoded.data.username}, (err, cart) => {
-    Cart.findOne({username: req.params.username}, (err, cart) => {
-        if (err) {
-            res.status(500).json(err);
-        } else if (!cart && quantity <= 0) {
-            res.status(400).json('Invalid request!');
+exports.getCart = async (req, res) => {
+    try {
+        const carts = await Cart.find({ username: req.user.username, status: 'active' });
+        let data = {
+            username: req.user.username,
+            total_price: 0,
+            items: carts.map(cart => {
+                return cart.item;
+            })
         }
-        else if (cart) {
-            const indexFound = cart.items.findIndex(item => {
-                return item.flower_id === flower_id;
-            });
-            if (indexFound !== -1 && quantity <= 0) {
-                cart.items.splice(indexFound, 1);
-            }
-            else if (indexFound !== -1) {
-                cart.items[indexFound].quantity += quantity;
-                cart.total_price += quantity * unit_price;
-            }
-            else if (quantity > 0) {
-                cart.items.push({
-                    flower_id: flower_id,
-                    quantity: quantity,
-                    unit_price: unit_price
+        data.items.forEach(item => {
+            data.total_price += item.quantity * item.unit_price;
+        });
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(404).json({ message: 'Cart not found!' })
+    }
+};
+
+exports.addItem = async (req, res) => {
+    if (req.body.quantity > 0) {
+        try {
+            const carts = await Cart.find({ username: req.user.username, status: 'active' });
+            if (carts) {
+                const indexFound = carts.findIndex(cart => {
+                    return cart.item.flower_id === req.body.flower_id;
                 });
-                cart.total_price += quantity * unit_price;
-            } else {
-                res.status(400).json('Invalid request!');
-            }
-            
-            cart.save((err, result) => {
-                if (err) {
-                    res.status(500).json(err);
+                if (indexFound > -1) {
+                    carts[indexFound].item.quantity += req.body.quantity;
+                    const result = await carts[indexFound].save();
+                    res.status(201).json(result);
+                } else {
+                    throw error = { message: 'Invalid request!' };
                 }
-                res.status(200).json({cart: result});
-            });
-        } else {
-            const cartData = {
-                username: req.jwtDecoded.data.username,
-                items: {
-                    flower_id: flower_id,
-                    quantity: quantity,
-                    unit_price: unit_price
-                },
-                total_price: quantity * unit_price
             }
-            const cart = new Cart(cartData);
-            cart.save((err, result) => {
-                if (err) {
-                    res.status(500).json(err);
-                }
-                res.status(200).json({cart: result});
-            });
+            const data = {
+                username: req.user.username,
+                item: req.body
+            };
+            const cart = new Cart(data);
+            const result = await cart.save();
+            res.status(201).json(result);
+        } catch (error) {
+            res.status(500).json(error);
         }
-    })
+    } else {
+        res.status(400).json('Quantity must be 1 at least!');
+    }
 };
 
-exports.deleteItem = (req, res) => {
-    //Cart.findOne({username: req.jwtDecoded.data.username})
-    Cart.findOne({username: req.params.username})
-        .then(cart => {
-            const indexFound = cart.items.findIndex(item => {
-                return item.flower_id == req.params.id;
+exports.deleteItem = async (req, res) => {
+    try {
+        const carts = await Cart.find({ username: req.user.username, status: 'active' });
+        if (carts) {
+            const indexFound = carts.findIndex(cart => {
+                return cart.item.flower_id === req.params.id;
             });
             if (indexFound > -1) {
-                cart.total_price -= cart.items[indexFound].quantity * cart.items[indexFound].unit_price;
-                cart.items.splice(indexFound, 1);
-                return cart.save();
+                carts[indexFound].status = 'disabled';
+                const result = await carts[indexFound].save();
+                res.status(200).json(result);
             } else {
-                res.status(400).json('Invalid request!');
+                throw error = { message: 'Invalid request!' };
             }
-        })
-        .then(result => {
-            res.status(200).json({cart: result});
-        })
-        .catch(error => {
-            res.json(error);
-        });
+        }
+    } catch (error) {
+        res.status(400).json(error);
+    }
 };
