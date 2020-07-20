23a50bbbda355a872267f80e8f9c315839813743
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-07-20 14:53:45 +0700

    Use promise for order.controllers.js

--- controllers/order.controllers.js
+++ controllers/order.controllers.js
@@ -2,44 +2,32 @@ const Order = require('../models/order');
 const Cart = require('../models/cart');
 const Flower = require('../models/flower');
 const mongoose = require('mongoose');
+const perPage = 20;
 
-exports.listOrders = (req, res) => {
-    Order.find({ username: req.role === "admin" ? req.params.username : req.jwtDecoded.data.username }, (err, orders) => {
-        if (err) {
-            res.status(500).json(err);
-        } else if (orders) {
-            res.status(200).json({ orders: orders });
+exports.listOrders = async (req, res) => {
+    try {
+        const page = req.query.page ? req.query.page - 1 : 0;
+        const orders = await Order.find({ username: req.user.username, status: 'active' }).limit(perPage).skip(perPage * page);
+        if (orders.length > 0) {
+            res.status(200).json(orders);
         } else {
-            res.status(404).json({ message: 'Order not found!' });
+            throw error;
         }
-    });
+    } catch (error) {
+        res.status(404).json({ message: 'Order not found!' });
+    }
 };
 
-exports.getOrder = (req, res) => {
-    Order.findOne({ username: req.role === "admin" ? req.params.username : req.jwtDecoded.data.username, _id: req.params.id }, (err, order) => {
-        if (err) {
-            res.status(500).json(err);
-        } else if (order) {
-            res.status(200).json({ order: order });
-        } else {
-            res.status(404).json({ message: 'Order not found!' });
-        }
-    })
+exports.getOrder = async (req, res) => {
+    try {
+        const order = await Order.findOne({ username: req.user.username, status: 'active', _id: req.params.id });
+        res.status(200).json({ order: order });
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid request!' });
+    }
 };
 
-exports.deleteOrder = (req, res) => {
-    Order.findOneAndRemove({ username: req.jwtDecoded.data.username, _id: req.params.id }, (err, result) => {
-        if (err) {
-            res.status(500).json(err);
-        } else if (result) {
-            res.status(200).json({ order: result });
-        } else {
-            res.status(400).json({ message: 'Invalid request!' });
-        }
-    });
-};
-
-exports.placeOrder = (req, res) => {
+exports.placeOrder = async (req, res) => {
     //res.json(req.jwtDecoded.data);
 
     if (!(Array.isArray(req.body.items) && req.body.items.length > 0)) {
@@ -47,147 +35,28 @@ exports.placeOrder = (req, res) => {
     }
     else {
         const orderData = {
-            username: req.jwtDecoded.data.username,
+            username: req.user.username,
             bill_address: req.body.bill_address,
             shipping_method: req.body.shipping_method,
             payment_method: req.body.payment_method,
             items: req.body.items,
-            total_price: orderData.items.reduce((total, item) => {
+            total_price: req.body.items.reduce((total, item) => {
                 return total + item.unit_price * item.quantity;
             }, 0)
         };
-        
-
-        const order = new Order(orderData);
-
-        //CALLBACK
-        //PROBLEM: MUST BE A NEST CALLBACK BECAUSE FLOWER.QUALITY MAY CAUSE ERROR DUE TO LIMITATION
-        /*    
-        orderData.items.forEach(item => {
-            Flower.findById(item.flower_id, (err, flower) => {
-                if (err) {
-                    res.status(500).json(err);
-                } else if (flower) {
-                    flower.quantity = flower.quantity - item.quantity;
-                    flower.save((err, flower) => {
-                        if (err) {
-                            res.status(500).json(err);
-                        }
-                    });
-                } else {
-                    res.status(404).json('Flower not found!');
-                }
-            });            
-        });
-        
-        Cart.findOne({username: orderData.username}, (err, cart) => {
-            if (err) {
-                res.json(err);
-            } else if (cart) {
-                orderData.items.forEach(item => {
-                    var indexFound = cart.items.findIndex(item_in_cart => {
-                        return item_in_cart.flower_id === item.flower_id;
-                    });
-                    if (indexFound > -1) {
-                        cart.items[indexFound].quantity = cart.items[indexFound].quantity - item.quantity;
-                        cart.total_price -= item.quantity * item.unit_price;
-                        if (cart.items[indexFound].quantity <= 0) {
-                            cart.items.splice(indexFound, 1);
-                        }                                     
-                    }               
-                });
-                cart.save((err, cart) => {
-                    if (err) {
-                        res.status(500).json(err);
-                    }
-                });            
-            } else {
-                res.status(404).json('Cart not found!');
-            }
-        });
-        order.save((err, order) => {
-            if (err) {
-                res.status(500).json(err);
-            } else {
-                res.status(201).json(order);
-            }
-        });
-        */
-
-
-
-
-
-
-        //PROMISE IN ORDER TO AVOID CALLBACK HELL
-        //PROBLEM: SAVING BEFORE ALTHOUGH UPDATING AFTER MAY CAUSE ERROR DUE TO THE EXISTENCE AND LIMITATION OF DATA
-        /*
-        order.save()
-            .then(savedOrder => {
-                var update_flower_promise = savedOrder.items.map(item => {
-                //savedOrder.items.map(item => {
-                    return Flower.findById(item.flower_id)
-                    .then(flower => {
-                        flower.quantity = flower.quantity - item.quantity;
-                        return flower.save();
-                    });
-                             
-                });
-                Promise.all(update_flower_promise)
-                    .then(result => {
-
-                        Cart.findOne({username: savedOrder.username})
-                            .then(cart => {
-                                if (cart) {
-                                    savedOrder.items.forEach(item => {
-                                        var indexFound = cart.items.findIndex(item_in_cart => {
-                                            return item_in_cart.flower_id === item.flower_id;
-                                        });
-                                        if (indexFound > -1) {
-                                            cart.items[indexFound].quantity = cart.items[indexFound].quantity - item.quantity;
-                                            cart.total_price -= item.quantity * item.unit_price;
-                                            if (cart.items[indexFound].quantity <= 0) {
-                                                cart.items.splice(indexFound, 1);
-                                            }                                     
-                                        }
-                                    });
-                                    return cart.save();
-                                }
-                            })
-                            .then(result => {
-                                res.json(savedOrder);
-                            })
-                            
-                            //.catch (error => {
-                            //    return error;
-                            //});               
-                             
-                            
-                    })
-                    //.catch(error => {
-                        //res.json(error);
-                    //    return error;
-                    //});
-                    });
-                      
-            })
-            .catch(error => {
-                //res.json(error);
-                return error;                
-            });
-            */
-
+        const order = new Order(orderData);        
         //transaction mongodb
-        //ANOTHER PROMISE
         const session = await mongoose.startSession();
         try {
             session.startTransaction();
-            const update_flower_promise = order.items.map(item => {
-                Flower.updateOne({ _id: item.flower_id }, { $inc: { quantity: -item.quantity } }, { session })
-
+            const update_flowers_promise = order.items.map(item => {
+                //Flower.findOneAndUpdate({ _id: item.flower_id, status: 'active' }, { $inc: { quantity: -item.quantity } }, { session: session });
+                Flower.findOneAndUpdate({ _id: item.flower_id, status: 'active' }, { $inc: { quantity: -item.quantity } }).session(session);
             });
-            const result = await Promise.all(update_flower_promise);
-            const cart = await Cart.findOne({ username: order.username },);
+            await Promise.all(update_flowers_promise);
+            //Solution 1:
+            /*
+            const cart = await Cart.findOne({ username: order.username, status: 'active' },);
             if (cart) {
                 order.items.forEach(item => {
                     const indexFound = cart.items.findIndex(item_in_cart => {
@@ -201,14 +70,26 @@ exports.placeOrder = (req, res) => {
                         }
                     }
                 });
-                await cart.save({session});
+                await cart.save({ session });
             }
+            */
+
+            //Solution 2:
             
-            await order.save({session})
-            session.commitTransaction();
-        } catch (err) {
-            session.abortTransaction();
-            res.status(500).json(err);
+            const update_carts_promise = order.items.map(item => {
+                //Cart.findOneAndUpdate({ username: order.username, status: 'active', flower_id: item.flower_id }, { $inc: { quantity: -item.quantity } }, { session: session });
+                Cart.findOneAndUpdate({ username: order.username, status: 'active', flower_id: item.flower_id }, { $inc: { quantity: -item.quantity } }).session(session);
+            })
+            await Promise.all(update_carts_promise);
+            /**/
+            const result = await order.save({ session: session });
+            await session.commitTransaction();
+            session.endSession();
+            res.status(201).json(result);
+        } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+            res.status(500).json(error);
         }
     }
 };
