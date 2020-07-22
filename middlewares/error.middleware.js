2423f91928d09080d998391034e0e3918692520b
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-07-22 15:12:42 +0700

    Write error middleware

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,9 @@
+const { GeneralError, BadRequest, NotFound, Unauthorized } = require('../utils/error');
+
+exports.handleError = (error, req, res, next) => {
+    if (error instanceof GeneralError) {
+        res.status(error.getCode()).send(error);
+    } else {
+        res.status(500).send(new GeneralError('Internal Server Error'));
+    }
+};
