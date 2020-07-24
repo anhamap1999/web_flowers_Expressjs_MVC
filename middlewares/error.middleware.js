696494953f65bcc5c454e4e6254500e8a0094060
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-07-24 08:04:38 +0700

    Update error middleware

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,9 +1,9 @@
-const { GeneralError, BadRequest, NotFound, Unauthorized } = require('../utils/error');
+const { GeneralError } = require('../utils/error');
 
 exports.handleError = (error, req, res, next) => {
     if (error instanceof GeneralError) {
         res.status(error.getCode()).send(error);
     } else {
-        res.status(500).send(new GeneralError('Internal Server Error'));
+        res.status(500).send(new GeneralError(error.message));
     }
 };
